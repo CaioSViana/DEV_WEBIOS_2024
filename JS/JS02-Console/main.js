1c944b3257b427163cb1d711cba93fc50d9293a9
CaioSViana <caio.csv26@gmail.com>
2024-10-10 21:36:25 -0300

    JavaScript

---
+++ JS/JS02-Console/main.js
@@ -0,0 +1,15 @@
+
+let um = 1
+console.log(um)
+
+var x = "Arroz"
+console.log(x)
+
+var nome1 = "Lukas ";
+var nome2 = "com K";
+
+var nomeCompleto = nome1 + nome2;
+var nomeCompleto2 = "Lukas " + "com K";
+
+console.log(nomeCompleto)
+console.log(nomeCompleto2)
