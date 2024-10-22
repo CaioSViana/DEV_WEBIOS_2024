358f0b52fb52261bcbd312d7c170e1a4181bf66a
CaioSViana <caio.csv26@gmail.com>
2024-10-21 21:42:15 -0300

    Condicionais

--- JS/JS02-Console/main.js
+++ JS/JS02-Console/main.js
@@ -1,15 +1,22 @@
+'use strict'
 
-let um = 1
-console.log(um)
+// let um = 1
+// console.log(um)
 
-var x = "Arroz"
-console.log(x)
+// var x = "Arroz"
+// console.log(x)
 
-var nome1 = "Lukas ";
-var nome2 = "com K";
+// var nome1 = "Lukas ";
+// var nome2 = "com K";
 
-var nomeCompleto = nome1 + nome2;
-var nomeCompleto2 = "Lukas " + "com K";
+// var nomeCompleto = nome1 + nome2;
+// var nomeCompleto2 = "Lukas " + "com K";
 
-console.log(nomeCompleto)
-console.log(nomeCompleto2)
+// console.log(nomeCompleto)
+// console.log(nomeCompleto2)
+
+// let nota1, nota2, media;
+// nota1 = 5
+// nota2 = 10
+// media = (nota1 + nota2)/2
+// console.log(media);
---
+++ JS/JS04-Condicionais/main.js
@@ -0,0 +1,31 @@
+// verificar se estou com a blusa do IOS
+
+
+// let CaioComBlusa = false
+
+// if(CaioComBlusa == true){
+//     console.log("Caio pode assistir a aula!")
+// }else{
+//     console.log("Caio você não vai assistir a aula e terá uma conversa com a mãe da Gabriella")
+// }
+
+
+let pontos = 1000;
+
+if(pontos >= 1000){
+    console.log("Passou para a fase 2")
+}
+
+console.clear()
+
+let imparPar = 23
+
+if(imparPar % 2 == 0){
+    console.log("Par")
+}else{
+    console.log("Impar")
+}
+
+
+
+
