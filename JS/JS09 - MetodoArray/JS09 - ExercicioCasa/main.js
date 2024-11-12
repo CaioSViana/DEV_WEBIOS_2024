fc53f3439c1516c0cba3f933401af4e4a361412e
CaioSViana <47906580+CaioSViana@users.noreply.github.com>
2024-11-11 23:38:42 -0300

    Update

--- JS/JS09 - MetodoArray/JS09 - ExercicioCasa/main.js
+++ JS/JS09 - MetodoArray/JS09 - ExercicioCasa/main.js
@@ -15,7 +15,7 @@ array1.forEach( array1 => {
 var array2 = [ 17, 43, 8, 4, 97, 56, 29, 3, 75, 34, 88, 82, 61, 59];
 
 var filter = array2.filter( (valor) =>{
-    return valor > 20 && valor
+    return valor > 20 && valor < 80
 })
 
 console.log(`Os números entre 20 e 80 são: ${filter}`);
